41a1865ca230f22a36c1cdcb07b4fcca7883ec3d
seanduignan <45176053+seanduignan@users.noreply.github.com>
2018-12-07 10:10:30 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -14,15 +14,38 @@ var livestockSchema = new Schema({
       CauseOfDeath: String,
       DateOfDeath: String
 });
+var feedSchema = new Schema({
+      Qty: String,
+      Price: String,
+      Supplier: String,
+      Code: String
+});
+var vetSchema = new Schema({
+      Reason: String,
+      Cost: String,
+      VetName: String,
+      Date: String,
+      Tag: String
+});
 
 var Livestock = mongoose.model('livestock', livestockSchema);
+var Feed = mongoose.model('feed', feedSchema);
+var Vet = mongoose.model('vet', vetSchema);
 
 router.get('/', function(req, res, next) {
   Livestock.find()
-      .then(function(doc) {
-        console.log("Array length is: " + doc.length);
-        res.render('index', {title: "Farm Page", items: doc});
-      });
+    .then(function(livestocks) {
+      Feed.find()
+        .then(function(feeds) {
+          Vet.find()
+           .then(function(vets) {
+          console.log("livestocks length is: " + livestocks.length);
+          console.log("feeds length is: " + feeds.length);
+          console.log("vets length is: " + vets.length);
+          res.render('index', {title: "Farm Page", livestockArray: livestocks, feedArray: feeds, vetArray: vets});
+        });
+    });
+});
 });
 
 module.exports = router;
